1f344c5e5534c53875e8455d5b496785dfd4af26
matiasencion <matiassencion@gmail.com>
2025-08-16 22:35:31 -0300

    correccion a tcp

--- xmlrpc_redes/xmlrpc_redes/operations.py
+++ xmlrpc_redes/xmlrpc_redes/operations.py
@@ -2,15 +2,14 @@ import socket
 import utils
 
 class Server:
-    my_socket = None
+    server_welcoming_socket = None
     my_address = None
     my_port = None
     procedures = {}
     def __init__(self, address, port):
-        self.my_address = address
-        self.my_port = port
-        self.my_socket = socket(AF_INET, SOCK_STREAM)
-        self.my_socket.bind((self.my_address, self.my_port))
+        self.server_welcoming_socket = socket(AF_INET, SOCK_STREAM)
+        self.server_welcoming_socket.bind((address, port))
+        self.my_address, self.my_port = self.server_welcoming_socket.getsockname()
         print('Servidor creado con IP: ' + self.my_address + ' y puerto: ' + self.my_port)
     
     def add_method(self, method):
@@ -23,52 +22,52 @@ class Server:
 
     def serve(self):
         while True:
+            self.server_welcoming_socket.listen(1) #SI EL SERVIDOR ESTA OCUPADO COMO MAXIMO PUEDE HABER UNA COLA DE 1 ESPERANDO
             print('El servidor esta listo para recibir.')
-            request_in_xml, cl_address_port = self.sv_socket.recvfrom()
+            connection_socket, cl_address_port = self.server_welcoming_socket.accept()
+            print('Conexion establecida con ' + cl_address_port)
+            request_in_xml = connection_socket.recv().decode()
             #PROCESAR XML
             #EJECUTAR FUNCION CON SUS PARAMETROS
             #PASAR A XML
             response_in_xml = None
-            self.my_socket.sendto(response_in_xml, cl_address_port)
-            print('Resultado enviado hacia el cliente.')
+            connection_socket.send(response_in_xml)
+            print('Resultado enviado hacia el cliente ' + cl_address_port)
+            connection_socket.close() # NO ES PERSISTENTE?
 
 class Connection:
-    my_address = None
-    my_port = None
-    my_socket = None
+    client_socket = None
     sv_address = None
     sv_port = None
-    def __init__(self, address, port, sv_address, sv_port):
-        self.my_address = address
-        self.my_port = port
+    request_in_xml = None
+    response_in_xml = None
+    def __init__(self, cl_socket, sv_address, sv_port):
+        self.client_socket = cl_socket
         self.sv_address = sv_address
         self.sv_port = sv_port
-        self.my_socket = socket(AF_INET, SOCK_STREAM)
-
-    def __call__(self, address, port, sv_address, sv_port): #SI QUIERE CAMBIAR LA DIRECCION IP O PUERTO DEL SERVIDOR y SU PROPIA IP O PUERTO
-        self.my_address = address
-        self.my_port = port
-        self.sv_address = sv_address
-        self.sv_port = sv_port
+        self.client_socket.connect((self.sv_address, self.sv_port))
+        print('Conexion establecida con ' + self.sv_address + ':' + self.sv_port)
 
     def __getattr__(self, method):
         def wrapper(*args):
-            None
-            self.request_in_xml = None
             #TRANSFORMAR FUNCION Y PARAMETROS A XML
-        self.my_socket.sendto(self.request_in_xml, (self.sv_address, self.sv_port))
-        print('Procedimiento enviado hacia el servidor.')
+            self.request_in_xml = None
+        self.client_socket.send(self.request_in_xml.encode())
+        print('Procedimiento enviado hacia el servidor ' + self.sv_address + ':' + self.sv_port)
         print('Esperando respuesta...')
-        response_in_xml, sv_address_port = self.my_socket.recvfrom()
+        self.response_in_xml = self.client_socket.recv().decode()
         #TRANSFORMAR XML A RESULTADOS
+        #HAY QUE VER SI SE CIERRA LA CONEXION O NO
 
 
 class Client:
+    client_socket = None
     my_address = None
     my_port = None
-    def __init__(self, address, port):
-        self.my_address = address
-        self.my_port = port
+    def __init__(self):
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.my_address, self.my_port = self.client_socket.getsockname()
+        print('Cliente creado con IP: ' + self.my_address + ' y puerto: ' + self.my_port)
 
     def connect(self, address, port):
-        return Connection(self.my_address, self.my_port, address, port)
+        return Connection(self.client_socket, address, port)
